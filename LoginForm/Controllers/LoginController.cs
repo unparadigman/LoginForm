88f66ff38483391159d71d428a678c320c508c87
unparadigman <Alberto.MarcosMartinez@gmail.com>
2022-05-05 16:43:00 +0200

    Basic login form that redirect to a MailPage App

---
+++ LoginForm/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿
+using LoginForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ModelClient modelClient)
+        {
+            var cadena = modelClient.Email;
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ LoginForm/Models/ModelClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginForm.Models
+{
+    public class ModelClient
+    {
+        [Display(Name = "Correo electrónico")]
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; }
+
+        [Display(Name="Número de Documento")]
+        [Required(ErrorMessage = "El número de documento es obligatorio")]
+        [MaxLength(9,ErrorMessage = "No puede tener más de 9 dígitos")]
+        public string DocumentoNumero { get; set; }
+
+        [Display(Name ="Password")]
+        [Required(ErrorMessage = "La password es obligatoria")]
+        public string Password { get; set; }
+    }
+}
